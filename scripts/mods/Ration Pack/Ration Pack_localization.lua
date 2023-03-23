73239bde9ac6a288e463c2d59a5e0236d5bb959c
deluxghost <deluxghost@users.noreply.github.com>
2023-03-23 13:20:34 +0800

    Update Ration Pack_localization.lua

--- scripts/mods/Ration Pack/Ration Pack_localization.lua
+++ scripts/mods/Ration Pack/Ration Pack_localization.lua
@@ -1,14 +1,18 @@
 return {
 	mod_description = {
 		en = "Sets the colour of the ammo crate icon to indicate how many charges left in the box",
+		["zh-cn"] = "设置弹药箱图标颜色，表示箱中剩余多少次弹药补给",
 	},
   mod_title = {
-    en = "Ration Pack"
+    en = "Ration Pack",
+    ["zh-cn"] = "补给计数",
   },
   show_numbers = {
-    en = "Show numbers"
+    en = "Show numbers",
+    ["zh-cn"] = "显示数字",
   },
   show_colours = {
-    en = "Show colours"
+    en = "Show colours",
+    ["zh-cn"] = "显示颜色",
   },
 }
