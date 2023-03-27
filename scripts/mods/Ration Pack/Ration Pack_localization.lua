75045da152b92d5aa93838715909de1f9112633d
Ben <Lai>
2023-03-27 12:11:15 +1100

    Russian Localisation from xsSplater

--- scripts/mods/Ration Pack/Ration Pack_localization.lua
+++ scripts/mods/Ration Pack/Ration Pack_localization.lua
@@ -2,17 +2,21 @@ return {
 	mod_description = {
 		en = "Sets the colour of the ammo crate icon to indicate how many charges left in the box",
 		["zh-cn"] = "设置弹药箱图标颜色，表示箱中剩余多少次弹药补给",
+		ru = "Отображает над разложенным ящиком боеприпасов цветную иконку, чтобы показать сколько осталось зарядов в коробке.",
 	},
-  mod_title = {
-    en = "Ration Pack",
-    ["zh-cn"] = "补给计数",
-  },
-  show_numbers = {
-    en = "Show numbers",
-    ["zh-cn"] = "显示数字",
-  },
-  show_colours = {
-    en = "Show colours",
-    ["zh-cn"] = "显示颜色",
-  },
+	mod_title = {
+		en = "Ration Pack",
+		["zh-cn"] = "补给计数",
+		ru = "Ration Pack",
+	},
+	show_numbers = {
+		en = "Show numbers",
+		["zh-cn"] = "显示数字",
+		ru = "Показать числа",
+	},
+	show_colours = {
+		en = "Show colours",
+		["zh-cn"] = "显示颜色",
+		ru = "Показать цвета",
+	},
 }
